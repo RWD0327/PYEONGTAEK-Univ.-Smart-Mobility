5f864b01f2956c0a108be9f4ae78ef4c854db60c
RWD0327 <kjw46220@ptu.ac.kr>
2024-12-15 15:00:21 +0900

    수정 수정 수정 버그해결 버그해결결

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    const hbbtn = document.querySelector('span');
     
-
+    hbbtn.addEventListener('click', function() {
+        document.querySelector('#menu')
+    });
 
 });
